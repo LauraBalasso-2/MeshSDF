e831d02a8faca512349b439c2bef197d4884665d
Laura <laura.balasso@optimad.it>
2024-01-16 10:35:04 +0100

    refactoring training

--- train_deep_sdf.py
+++ train_deep_sdf.py
@@ -27,8 +27,6 @@ def main_function(experiment_directory, resolution):
 
     lr_schedules = get_learning_rate_schedules(specs)
 
-    grad_clip = get_spec_with_default(specs, "GradientClipNorm", None)
-
     def save_latest(epoch):
         save_model(experiment_directory, "latest.pth", decoder, epoch)
         save_optimizer(experiment_directory, "latest.pth", optimizer_all, epoch)
@@ -48,13 +46,9 @@ def main_function(experiment_directory, resolution):
     num_samp_per_scene = specs["SamplesPerScene"]
     subsample_random_seed = specs["RandomSubsampleSeed"]
     scene_per_batch = specs["ScenesPerBatch"]
-    clamp_dist = specs["ClampingDistance"]
-    minT = -clamp_dist
-    maxT = clamp_dist
     enforce_minmax = True
 
-    do_code_regularization = get_spec_with_default(specs, "CodeRegularization", True)
-    code_reg_lambda = get_spec_with_default(specs, "CodeRegularizationLambda", 1e-4)
+
 
     code_bound = get_spec_with_default(specs, "CodeBound", None)
     decoder = DeepSDF(latent_size, **specs["NetworkSpecs"]).cpu()
@@ -135,49 +129,21 @@ def main_function(experiment_directory, resolution):
         )
     )
     # train parameterization
+
+    test_loss = []
+
     for epoch in range(start_epoch, num_epochs + 1):
 
-        decoder.train()
         adjust_learning_rate(lr_schedules, optimizer_all, epoch)
 
-        for sdf_data, indices, names in sdf_loader:
-            optimizer_all.zero_grad()
+        train(decoder, sdf_loader, optimizer_all, epoch, specs, enforce_minmax, num_samp_per_scene, loss_l1, loss_log,
+              lat_vecs)
 
-            sdf_data = sdf_data.reshape(-1, 4)
-            num_sdf_samples = sdf_data.shape[0]
-            sdf_data.requires_grad = False
-            xyz = sdf_data[:, 0:3]
-            sdf_gt = sdf_data[:, 3].unsqueeze(1)
-
-            if enforce_minmax:
-                sdf_gt = torch.clamp(sdf_gt, minT, maxT)
-
-            indices = indices.unsqueeze(-1).repeat(1, num_samp_per_scene).view(-1)
-            batch_vecs = lat_vecs(indices.cpu())
-
-            pred_sdf = decoder(batch_vecs, xyz.cpu())
-
-            if enforce_minmax:
-                pred_sdf = torch.clamp(pred_sdf, minT, maxT)
-
-            batch_loss = loss_l1(pred_sdf, sdf_gt.cpu()) / num_sdf_samples
-
-            if do_code_regularization:
-                l2_size_loss = torch.sum(torch.norm(batch_vecs, dim=1))
-                reg_loss = (
-                                   code_reg_lambda * min(1, epoch / 100) * l2_size_loss
-                           ) / num_sdf_samples
-
-                batch_loss = batch_loss + reg_loss.cpu()
-
-            batch_loss.backward()
-
-            loss_log.append(batch_loss.item())
-
-            if grad_clip is not None:
-                torch.nn.utils.clip_grad_norm_(decoder.parameters(), grad_clip)
-
-            optimizer_all.step()
+        # decoder.eval()
+        # with torch.no_grad():
+        #
+        #     for sdf_test_data, test_indices, test_names in sdf_loader_test:
+        #         pass
 
         print("epoch {}...".format(epoch))
 
@@ -189,7 +155,7 @@ def main_function(experiment_directory, resolution):
                 epoch,
             )
 
-    # store reconstructions
+    # store reconstruction
     # decoder.eval()
     # reconstruction_dir = get_reconstruction_dir(experiment_directory, True)
     # for sdf_data, indices, name in sdf_loader_reconstruction:
@@ -200,6 +166,57 @@ def main_function(experiment_directory, resolution):
     #         create_mesh(decoder, latent, N=resolution, output_mesh=False, filename=mesh_filename)
 
     print("Done!")
+
+
+def train(decoder, sdf_loader, optimizer_all, epoch, specs, enforce_minmax, num_samp_per_scene, loss_l1, loss_log,
+          lat_vecs):
+
+    decoder.train()
+
+    grad_clip = get_spec_with_default(specs, "GradientClipNorm", None)
+    clamp_dist = specs["ClampingDistance"]
+    minT = -clamp_dist
+    maxT = clamp_dist
+    do_code_regularization = get_spec_with_default(specs, "CodeRegularization", True)
+    code_reg_lambda = get_spec_with_default(specs, "CodeRegularizationLambda", 1e-4)
+    for sdf_data, indices, names in sdf_loader:
+        optimizer_all.zero_grad()
+
+        sdf_data = sdf_data.reshape(-1, 4)
+        num_sdf_samples = sdf_data.shape[0]
+        sdf_data.requires_grad = False
+        xyz = sdf_data[:, 0:3]
+        sdf_gt = sdf_data[:, 3].unsqueeze(1)
+
+        if enforce_minmax:
+            sdf_gt = torch.clamp(sdf_gt, minT, maxT)
+
+        indices = indices.unsqueeze(-1).repeat(1, num_samp_per_scene).view(-1)
+        batch_vecs = lat_vecs(indices.cpu())
+
+        pred_sdf = decoder(batch_vecs, xyz.cpu())
+
+        if enforce_minmax:
+            pred_sdf = torch.clamp(pred_sdf, minT, maxT)
+
+        batch_loss = loss_l1(pred_sdf, sdf_gt.cpu()) / num_sdf_samples
+
+        if do_code_regularization:
+            l2_size_loss = torch.sum(torch.norm(batch_vecs, dim=1))
+            reg_loss = (
+                               code_reg_lambda * min(1, epoch / 100) * l2_size_loss
+                       ) / num_sdf_samples
+
+            batch_loss = batch_loss + reg_loss.cpu()
+
+        batch_loss.backward()
+
+        if grad_clip is not None:
+            torch.nn.utils.clip_grad_norm_(decoder.parameters(), grad_clip)
+
+        optimizer_all.step()
+
+        loss_log.append(batch_loss.item())
 
 
 if __name__ == "__main__":
