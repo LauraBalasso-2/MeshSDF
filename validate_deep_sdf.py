47a2511235576c108ef407c0bb1b73483cf586af
Laura <laura.balasso@optimad.it>
2024-02-06 11:32:36 +0100

    add error plots

--- validate_deep_sdf.py
+++ validate_deep_sdf.py
@@ -1,4 +1,7 @@
-from lib import predict_sdf, compute_reconstruction_error
+import os.path
+
+from lib import predict_sdf, Logger
+from lib.error_computation import compute_reconstruction_error
 import argparse
 
 arg_parser = argparse.ArgumentParser(description="Compute validation errors")
@@ -14,12 +17,17 @@ arg_parser.add_argument(
 
 args = arg_parser.parse_args()
 
+logger_path = os.path.join(args.experiment_directory, 'sdf_prediction.log')
 
+# predict_sdf(args.experiment_directory, mesh_reconstruction=False, sample_error_plot=False, error_summary_plot='hist')
 
-predict_sdf(args.experiment_directory, mesh_reconstruction=True)
-
-# predict_sdf(args.experiment_directory, validation=True, mesh_reconstruction=True)
+# predict_sdf(args.experiment_directory,
+#             validation=True,
+#             mesh_reconstruction=True,
+#             sample_error_plot=True,
+#             error_summary_plot='bar',
+#             logger=Logger(logger_path, level='debug'))
 
 # compute_reconstruction_error(args.experiment_directory)
 
-# compute_reconstruction_error(args.experiment_directory, validation=True)
+#  compute_reconstruction_error(args.experiment_directory, validation=True)
