cbaf6c5b81a714f5125227304916799a0643e276
Laura <laura.balasso@optimad.it>
2024-02-06 11:33:59 +0100

    2 and 4 points interpolation

--- latent_space_interpolation.py
+++ latent_space_interpolation.py
@@ -2,7 +2,10 @@ import argparse
 
 import numpy as np
 import os
+import json
 import sys
+
+import matplotlib.pyplot as plt
 
 import torch
 
@@ -25,40 +28,51 @@ args = arg_parser.parse_args()
 experiment_directory = args.experiment_directory
 latent_vectors = load_latent_vectors(experiment_directory, checkpoint='latest')
 
-code_4 = latent_vectors[4]
-code_7 = latent_vectors[7]
-
-code_mean = torch.mean(torch.stack([code_4, code_7]), dim=0)
-code_025 = torch.mean(torch.stack([code_4, code_mean]), dim=0)
-code_075 = torch.mean(torch.stack([code_7, code_mean]), dim=0)
-# code_075 = np.mean([code_mean, code_7.numpy().reshape(-1, 1)], axis=0)
+np_latent = latent_vectors.numpy()
 
 
-# code_mean = np.mean([code_4.numpy().reshape(-1, 1), code_7.numpy().reshape(-1, 1)], axis=0)
+with open('/home/laura/exclude_backup/pycharm_projects_deployment/MeshSDF/splits/drivaer_2var_randomSplit_train.json', 'r') as f:
+    split = json.load(f)
+
+train_samples = split.get('sdf_samples').get('npz')
+
+plt.scatter(np_latent[:, 0], np_latent[:, 1])
+i_min = np.argmin(np_latent[:, 0])
+i_max = np.argmax(np_latent[:, 0])
+
+for i, t in enumerate(sorted(train_samples)):
+    plt.annotate(str(int(t[-3:])), (np_latent[i, 0], np_latent[i, 1]))
+plt.scatter([np_latent[i_min, 0], np_latent[i_max, 0]], [np_latent[i_min, 1], np_latent[i_max, 1]], color='red')
+plt.savefig(os.path.join(experiment_directory, 'latent_space_2.png'))
+
+# code_min = latent_vectors[i_min]
+# code_max = latent_vectors[i_max]
 #
-# code_025 = np.mean([code_4.numpy().reshape(-1, 1), code_mean], axis=0)
-# code_075 = np.mean([code_mean, code_7.numpy().reshape(-1, 1)], axis=0)
-
-decoder, epoch = load_decoder(experiment_directory, 'latest')
-decoder.eval()
-
-intermediate_codes = [code_025, code_mean, code_075]
-file_names = ['']
-
-x = np.random.uniform(-1, 1, size=250000)
-y = np.random.uniform(-0.5, 0.5, size=250000)
-z = np.random.uniform(-0.3, 0.3, size=250000)
-xyz = torch.as_tensor(np.column_stack([x, y, z]), dtype=torch.float)
-save_dir = os.path.join(experiment_directory, 'latent_space_interpolation')
-if not os.path.isdir(save_dir):
-    os.mkdir(save_dir)
-
-for i, code in enumerate(intermediate_codes):
-    predicted_sdf = decode_sdf(decoder, code, xyz)
-    data_pred = {'sdf_predicted': np.ascontiguousarray(predicted_sdf.detach().numpy().reshape(-1)),
-                 'x': np.ascontiguousarray(xyz[:, 0]),
-                 'y': np.ascontiguousarray(xyz[:, 1]),
-                 'z': np.ascontiguousarray(xyz[:, 2])}
-    save_vtu(os.path.join(save_dir, 'predicted_sdf_'+str(i).zfill(2)), data_pred)
-
-    reconstruct_mesh(decoder, 'reconstruction_'+str(i).zfill(2), code, save_dir)
+# code_mean = torch.mean(torch.stack([code_min, code_max]), dim=0)
+# code_025 = torch.mean(torch.stack([code_min, code_mean]), dim=0)
+# code_075 = torch.mean(torch.stack([code_max, code_mean]), dim=0)
+#
+#
+# decoder, epoch = load_decoder(experiment_directory, 'latest')
+# decoder.eval()
+#
+# intermediate_codes = [code_025, code_mean, code_075]
+# file_names = ['']
+#
+# x = np.random.uniform(-1, 1, size=250000)
+# y = np.random.uniform(-0.5, 0.5, size=250000)
+# z = np.random.uniform(-0.3, 0.3, size=250000)
+# xyz = torch.as_tensor(np.column_stack([x, y, z]), dtype=torch.float)
+# save_dir = os.path.join(experiment_directory, 'latent_space_interpolation')
+# if not os.path.isdir(save_dir):
+#     os.mkdir(save_dir)
+#
+# for i, code in enumerate(intermediate_codes):
+#     predicted_sdf = decode_sdf(decoder, code, xyz)
+#     data_pred = {'sdf_predicted': np.ascontiguousarray(predicted_sdf.detach().numpy().reshape(-1)),
+#                  'x': np.ascontiguousarray(xyz[:, 0]),
+#                  'y': np.ascontiguousarray(xyz[:, 1]),
+#                  'z': np.ascontiguousarray(xyz[:, 2])}
+#     save_vtu(os.path.join(save_dir, 'predicted_sdf_'+str(i).zfill(2)), data_pred)
+#
+#     reconstruct_mesh(decoder, 'reconstruction_'+str(i).zfill(2), code, save_dir)
